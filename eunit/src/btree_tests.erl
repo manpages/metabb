0ac2a7c24d786958904eef5d2a0f053719e738a5
Jonns Mostovoys <python@udaff.com>
2011-09-01 14:54:27 +0300

    btree done

--- eunit/src/btree_tests.erl
+++ eunit/src/btree_tests.erl
@@ -1,5 +1,25 @@
 -module(btree_tests).
 
+-include_lib("eunit/include/eunit.hrl").
+
 -compile(export_all).
 
-f_test() -> btree:f(42).
+mktree_simple_test() ->
+	?debugHere,
+	?assert (btree:mktree("A") =:= "A"),
+ok.
+
+mktree_tree_test() ->
+	?debugHere,
+	?assert (btree:mktree("A|B&(foo bar|D)") =:= 
+		{"&", 
+			{"|", "A", "B"}, 
+			{"|", "foo bar", "D"}
+		}
+	),
+ok.
+
+mktree_malformed_test() ->
+	?debugHere,
+	?assertException (_, {syntax, _}, btree:mktree("A|")),
+ok.
